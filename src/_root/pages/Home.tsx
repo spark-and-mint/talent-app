cebd22d1a145cbc118daa74a43e5b3d8c6bdff3d
Kevin <hello@kevinivan.com>
2024-05-11 18:53:53 +0200

    remove flags

--- src/_root/pages/Home.tsx
+++ src/_root/pages/Home.tsx
@@ -39,7 +39,7 @@ const Home = () => {
         />
       </div>
 
-      {!accepted && !member.importedAnswers ? (
+      {accepted && member.importedAnswers ? (
         <Link
           to="https://sparkmintfractional.slack.com/"
           target="_blank"
