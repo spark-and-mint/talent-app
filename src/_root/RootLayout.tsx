f5f7ba0647748ea1966f1bdb438050c98c31f89f
Kevin <hello@kevinivan.com>
2024-06-20 16:35:51 +0200

    remove status check

--- src/_root/RootLayout.tsx
+++ src/_root/RootLayout.tsx
@@ -5,8 +5,6 @@ import { useMemberContext } from "@/context/AuthContext"
 import { useUpdateMember } from "@/lib/react-query/queries"
 import { useState } from "react"
 import { Outlet, useLocation } from "react-router-dom"
-import { AnnoyedIcon, RefreshCcw } from "lucide-react"
-import { Button } from "@/components/ui"
 import { cn } from "@/lib/utils"
 import ServerError from "@/components/shared/ServerError"
 import acceptedMembers from "@/lib/constants/acceptedMembers"
@@ -18,8 +16,6 @@ const RootLayout = () => {
   const { mutateAsync: updateMember } = useUpdateMember()
   const location = useLocation()
   const projectPage = location.pathname.includes("/project/")
-  const goToDashboard =
-    member.status === "in review" || member.status === "accepted"
 
   const checkMemberStatus = async () => {
     try {
@@ -73,42 +69,16 @@ const RootLayout = () => {
     checkMemberStatus()
   }
 
-  if (goToDashboard) {
-    return (
-      <div className="container h-full">
-        <div className="flex flex-col space-y-8 lg:flex-row lg:space-x-12 lg:space-y-0">
-          {!projectPage && (
-            <aside className="lg:w-1/4">
-              <SidebarNav />
-            </aside>
-          )}
-          <div className={cn("flex-1", !projectPage && "lg:max-w-2xl")}>
-            <Outlet />
-          </div>
-        </div>
-      </div>
-    )
-  }
-
   return (
     <div className="container h-full">
-      <div className="flex flex-col gap-4 mt-24 text-center">
-        <AnnoyedIcon
-          strokeWidth={1.5}
-          className="w-12 h-12 mx-auto text-primary"
-        />
-        <h4 className="h4">Hmm, something went wrong.</h4>
-        <p className="leading-6 text-muted-foreground">
-          If this issue persists, please contact us on Slack or at{" "}
-          <a className="text-primary" href="mailto:hello@teamspark.xyz">
-            hello@teamspark.xyz
-          </a>
-        </p>
-        <div className="mt-4">
-          <Button onClick={() => window.location.reload()}>
-            <RefreshCcw className="w-4 h-4 mr-2" />
-            Reload page
-          </Button>
+      <div className="flex flex-col space-y-8 lg:flex-row lg:space-x-12 lg:space-y-0">
+        {!projectPage && (
+          <aside className="lg:w-1/4">
+            <SidebarNav />
+          </aside>
+        )}
+        <div className={cn("flex-1", !projectPage && "lg:max-w-2xl")}>
+          <Outlet />
         </div>
       </div>
     </div>
